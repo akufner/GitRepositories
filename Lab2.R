670d1a7e28edf6843829f8c2affb1cba895a5230
akufner <akufner@wisc.edu>
2016-02-07 13:59:31 -0600

    Week of Feb. 8 Assignments

---
+++ Lab2.R
@@ -0,0 +1,31 @@
+#PART 1
+#1. 10 different species
+#2. based on ridges, shape of cross section(circular or square), appearance of "coiling" pattern
+#3. Some of the ammonites appear to be miniature versions of one of the larger specimens.
+#4. Looked at the width of the shells since it has been established females tend to be larger.
+
+#PART 2
+#1. Land, links, site, species, outline
+#2. character
+#3. I can't.
+#Subsection 2
+#1. land
+#2. ok
+#3. you got it!
+#4. There is either one with a lot of variation or 10+ species that are very closely related.
+
+#PART 3
+#1. Fangs longer than 6 inches
+#2. Sulfurous odor
+#3. Adorable eyelashes
+#4. C, D, E
+#5. Laser death ray in species E
+#6. Synapomorphy
+#7. Family 1 - monophyletic, Family 2 - polyphyletic, Family 3 - monophyletic
+#1. No, that would be a paraphyletic group since B shares a more recent common ancestor with A, and C with D and E.
+#2. Group 1 - para, Group 2 - mono, Group 3 - para, Group 4 - mono, Group 5 - poly
+
+#PART 4
+#1. paedomorphosis
+#2. Gryphaea gigantea
+#3. peramorphosis
