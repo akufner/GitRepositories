c61031f2bf7541e5d34aa550aa670757f55a1876
paleobiologyGrader <aazaff3@gmail.com>
2016-02-12 15:56:40 -0600

    Update Lab2.R

--- Lab2.R
+++ Lab2.R
@@ -1,31 +1,71 @@
 #PART 1
+
+> This was a somewhat disappointing effort. If you were having trouble with R you should have asked for help. Your final grade is 15.6/20
+
 #1. 10 different species
+
+> You did not state which specimens belong to which species! - 1 points
+
 #2. based on ridges, shape of cross section(circular or square), appearance of "coiling" pattern
+
 #3. Some of the ammonites appear to be miniature versions of one of the larger specimens.
+
+> Fair enough, but you could have read about the degree of coiling, -0.5 points
+
 #4. Looked at the width of the shells since it has been established females tend to be larger.
 
 #PART 2
 #1. Land, links, site, species, outline
+
 #2. character
+
+> The correct answer is array, matrix, factor, factor, matrix, -0.5 points
+
 #3. I can't.
+
+> dim(plethodon[["land"]]), -0.5 points
+
 #Subsection 2
 #1. land
+
 #2. ok
+
+> What code did you use? -1 points
+
 #3. you got it!
+
+> How did you do it? -1 points
+
 #4. There is either one with a lot of variation or 10+ species that are very closely related.
 
+> Based on what?
+
 #PART 3
+
 #1. Fangs longer than 6 inches
+
 #2. Sulfurous odor
+
 #3. Adorable eyelashes
+
 #4. C, D, E
+
 #5. Laser death ray in species E
+
 #6. Synapomorphy
+
 #7. Family 1 - monophyletic, Family 2 - polyphyletic, Family 3 - monophyletic
+
 #1. No, that would be a paraphyletic group since B shares a more recent common ancestor with A, and C with D and E.
+
 #2. Group 1 - para, Group 2 - mono, Group 3 - para, Group 4 - mono, Group 5 - poly
 
 #PART 4
+
 #1. paedomorphosis
+
+> - 0.5
+
 #2. Gryphaea gigantea
+
 #3. peramorphosis
